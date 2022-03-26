7b2abfdbfc63669b866932298bbe5852ddb45af3
Brenner Spear <blspear@gmail.com>
2022-03-26 12:51:39 -0500

    Ok I think all linting stuff is done here

--- components/List.tsx
+++ components/List.tsx
@@ -1,19 +1,19 @@
-import * as React from 'react'
-import ListItem from './ListItem'
 import { User } from '../interfaces'
+import ListItem from './ListItem'
+import * as React from 'react'
 
 type Props = {
-  items: User[]
+    items: User[]
 }
 
 const List = ({ items }: Props) => (
-  <ul>
-    {items.map((item) => (
-      <li key={item.id}>
-        <ListItem data={item} />
-      </li>
-    ))}
-  </ul>
+    <ul>
+        {items.map((item) => (
+            <li key={item.id}>
+                <ListItem data={item} />
+            </li>
+        ))}
+    </ul>
 )
 
 export default List
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import Link from 'next/link'
-
 import { User } from '../interfaces'
+import Link from 'next/link'
+import React from 'react'
 
 type Props = {
-  data: User
+    data: User
 }
 
 const ListItem = ({ data }: Props) => (
-  <Link href="/users/[id]" as={`/users/${data.id}`}>
-    <a>
-      {data.id}: {data.name}
-    </a>
-  </Link>
+    <Link href="/users/[id]" as={`/users/${data.id}`}>
+        <a>
+            {data.id}: {data.name}
+        </a>
+    </Link>
 )
 
 export default ListItem
