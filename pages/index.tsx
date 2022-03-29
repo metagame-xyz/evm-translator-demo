3d596de45af1d8322dcbef10ca8b45af21682713
Brenner Spear <blspear@gmail.com>
2022-03-28 23:18:34 -0500

    1 level down

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const IndexPage = () => {
             )
 
             console.log(data)
-            setTxData(data)
+            setTxData(data.tx)
             setIsLoading(false)
         }
     }
